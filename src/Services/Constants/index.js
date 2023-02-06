263fcd16540d7299b5df1cf3676aeb239dbc8c16
ghuioio <54397393+ghuioio@users.noreply.github.com>
2023-02-06 22:27:01 +0700

    Update index.js

--- src/Services/Constants/index.js
+++ src/Services/Constants/index.js
@@ -2,6 +2,8 @@ export const BASE_API = 'https://books.monsters.vn/apis'
 export const BASE_FILE = 'https://books.monsters.vn/files'
 // export const HEROKU_API = "https://book-ecommerce-be.herokuapp.com/api"
 export const HEROKU_API = "http://localhost:8080/api"
+export const PDF_API = "http://localhost:5000"
+export const VOICE_API = "https://tjervfsstu4zexu4hbabzqolq40zudgs.lambda-url.ap-southeast-1.on.aws"
 
 export const PrevChooseAddress = {
   CHECK_OUT: 1,
